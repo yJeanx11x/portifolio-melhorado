b6faf3578c6a5b8b9b8df79f666cfce70b53cdc8
Jean Lucas <j3anlucas010@gmail.com>
2025-02-08 10:42:51 -0300

    finalizando o dark mode

--- script/script.js
+++ script/script.js
@@ -2,5 +2,7 @@ const dark_mode=document.querySelector('.dark_mode')
 dark_mode.addEventListener('click',()=>{
 
         const changeBodyColor=document.body
+
+        changeBodyColor.classList.toggle('trocarParaSol')
         changeBodyColor.classList.toggle('change_body_color')
 })
