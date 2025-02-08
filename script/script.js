ba3b86d68ba68523b9deb22e5650351838ff78aa
Jean Lucas <j3anlucas010@gmail.com>
2025-02-08 11:41:27 -0300

    animação do social network

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
+// Dark MODE
+
 const dark_mode=document.querySelector('.dark_mode')
 dark_mode.addEventListener('click',()=>{
 
         const changeBodyColor=document.body
-
+        
+        // QUANDO CLICAR MUDA PARA O TEMA BLACK
         changeBodyColor.classList.toggle('trocarParaSol')
         changeBodyColor.classList.toggle('change_body_color')
 })
