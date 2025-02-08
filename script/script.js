fad132146d68bc9730e0bae81d4ffc72992dce9f
Jean Lucas <j3anlucas010@gmail.com>
2025-02-07 22:25:13 -0300

    dark_mode

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const dark_mode=document.querySelector('.dark_mode')
+dark_mode.addEventListener('click',()=>{
+
+        const changeBodyColor=document.body
+        changeBodyColor.classList.toggle('change_body_color')
+})
